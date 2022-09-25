98cf61503c1a3c15d5f9a78907d7e3524fa7f636
HordesOfGhost <85671929+HordesOfGhost@users.noreply.github.com>
2022-09-25 17:45:24 +0545

    products problem

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,21 @@
-import React from "react";
-import { Link, useParams } from "react-router-dom";
+import React,{useState,useEffect} from 'react'
+import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
 
-function ProductScreen() {
-  const { id } = useParams();
-  const product = products.find((p) => p._id === id);
+import axios from 'axios'
+
+
+
+function ProductScreen({match}) {
+  const[product,setProduct]=useState([])
+  useEffect(()=>{
+    async function fetchProduct(){
+      const {data} =await axios.get(`/api/products/${match.params.id}`)
+      setProduct(data)
+    }
+    fetchProduct()
+  },[match.params.id])
   return (
     <div>
       <Link to="/" className="btn btn-light my-3">
