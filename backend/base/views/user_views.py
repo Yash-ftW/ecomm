ec24781fb933413a2e5d328ef46e80492c4bc5cd
HordesOfGhost <85671929+HordesOfGhost@users.noreply.github.com>
2022-09-26 20:22:25 +0545

    Completed 6

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+
 from django.contrib.auth.models import User
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.permissions import IsAuthenticated,IsAdminUser
