c89763b417d99dddcc4ba42622335ec271cebc25
Fluffy <87795895+MrFluffy-ftW@users.noreply.github.com>
2022-09-30 09:32:25 +0545

    Order Screen Completed

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -99,7 +99,9 @@ function OrderScreen() {
                 {order.paymentMethod}
               </p>
               {order.isPaid ? (
-                <Message variant="success">Paid on {order.paidAt}</Message>
+                <Message variant="success">
+                  Paid on {order.paidAt.substring(0, 10)}
+                </Message>
               ) : (
                 <Message variant="warning">Not Paid</Message>
               )}
