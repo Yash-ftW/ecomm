e4bd40d1f459d55ab60c71d7440f1c83bfbf83d6
HordesOfGhost <85671929+HordesOfGhost@users.noreply.github.com>
2022-10-14 16:24:32 +0545

    11.2 and 11.3 complete

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,17 +1,18 @@
 from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
+from traitlets import default
 # Create your models here.
 
 
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=100, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True,default='/placeholder.png')
     brand = models.CharField(max_length=100, null=True, blank=True)
     category = models.CharField(max_length=100, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    rating = models.DecimalField(max_digits=7, decimal_places=2)
+    rating = models.DecimalField(max_digits=7, decimal_places=2,null=True)
     numReviews = models.IntegerField(null=True, blank=True, default=0)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     countInStock = models.IntegerField(null=True, blank=True, default=0)
