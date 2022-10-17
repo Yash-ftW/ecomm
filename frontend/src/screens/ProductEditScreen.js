11227b56a5dac1db7ab45c092350b1e08761eafb
Fluffy <87795895+MrFluffy-ftW@users.noreply.github.com>
2022-10-17 14:09:59 +0545

    Completed 11.7

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -73,8 +73,31 @@ function ProductEditScreen() {
   };
 
   const uploadFileHandler = async (e) => {
-    const file = e.target.file[0];
+    const file = e.target.files[0];
     const formData = new FormData();
+
+    formData.append("image", file);
+    formData.append("product_id", productId);
+
+    setUploading(true);
+
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      };
+
+      const { data } = await axios.post(
+        "/api/products/upload/",
+        formData,
+        config
+      );
+      setImage(data);
+      setUploading(false);
+    } catch (error) {
+      setUploading(false);
+    }
   };
 
   return (
@@ -126,7 +149,6 @@ function ProductEditScreen() {
               <Form.Control
                 type="file"
                 label="Choose File"
-                custom
                 onChange={uploadFileHandler}
               ></Form.Control>
 
