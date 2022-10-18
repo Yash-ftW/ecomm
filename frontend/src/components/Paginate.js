8bec4cb130866c10d64f2aad6c46144ad23bc128
Fluffy <87795895+MrFluffy-ftW@users.noreply.github.com>
2022-10-18 12:27:01 +0545

    Pagination End

--- frontend/src/components/Paginate.js
+++ frontend/src/components/Paginate.js
@@ -1,16 +1,29 @@
 import React from "react";
-import { Pagination } from 'react-bootstrap'
-import {LinkContainer} from 'react-router-bootstrap'
+import { Pagination } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
-function Paginator(pages,page ,keyword='',isAdmin=false){
-    
-    return(pages>1 &&(
-        <Pagination>
-            
-        </Pagination>
+function Paginate({ pages, page, keyword = "", isAdmin = false }) {
+  if (keyword) {
+    keyword = keyword.split("?keyword=")[1].split("&")[0];
+  }
+
+  return (
+    pages > 1 && (
+      <Pagination>
+        {[...Array(pages).keys()].map((x) => (
+          <LinkContainer
+            key={x + 1}
+            to={
+              !isAdmin
+                ? `/?keyword=${keyword}&page=${x + 1}`
+                : `/admin/productlist/?keyword=${keyword}&page=${x + 1}`
+            }
+          >
+            <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
+          </LinkContainer>
+        ))}
+      </Pagination>
     )
-        
-    )
+  );
 }
-
-export default Paginate
+export default Paginate;
