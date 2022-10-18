0bbb1cf3da023b88dbfe366668402259cacf5f01
Fluffy <87795895+MrFluffy-ftW@users.noreply.github.com>
2022-10-18 16:52:14 +0545

    Hero

--- backend/settings.py
+++ backend/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+#import django_heroku
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -183,4 +183,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOW_ALL_ORIGINS=True
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
+
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
